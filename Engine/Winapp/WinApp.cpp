01abe5d90b98b27d5a2f917dad25756409f108fb
FujiiKohaku <k024g0078@g.neec.ac.jp>
2026-09-09 14:41:23 +0900

    a

--- Engine/Winapp/WinApp.cpp
+++ Engine/Winapp/WinApp.cpp
@@ -206,7 +206,7 @@ void WinApp::initialize()
     // ウィンドウ生成
     hwnd_ = CreateWindow(
         wc_.lpszClassName, // クラス名
-        L"死因：最適解", // タイトル
+        L"3029_死因：最適解", // タイトル
         kWindowStyle, // スタイル
         CW_USEDEFAULT, CW_USEDEFAULT, // 位置（自動）
         wrc.right - wrc.left, // 幅
